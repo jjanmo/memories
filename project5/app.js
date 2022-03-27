b94affbac5b7ef051c90df611a2405028ae6e15d
jjanmo <jjanmo07@hanmail.net>
2022-03-27 23:45:21 +0900

    feat: add feature of side button about when clicked image appear

--- project5/app.js
+++ project5/app.js
@@ -1,12 +1,35 @@
 const $hamberger = document.querySelector('.hamberger');
+const $buttonContainer = document.querySelector('.button-container');
+const $pictures = document.querySelectorAll('.picture');
 
-const handleClickHamberger = (e) => {
+const handleClickHamberger = () => {
   const $wrapper = document.querySelector('.wrapper');
   $wrapper.classList.toggle('clicked');
 };
 
+const handleClickButton = (e) => {
+  const className = e.target.className;
+  if (className.includes('captain')) {
+    const $captainPicture = document.querySelector('.captain-picture');
+    $captainPicture.classList.add('show');
+  } else if (className.includes('ironman')) {
+    const $ironmanPicture = document.querySelector('.ironman-picture');
+    $ironmanPicture.classList.add('show');
+  }
+};
+
+const handleClickPicture = (e) => {
+  const target = e.target.parentNode;
+  console.log(target);
+  target.classList.remove('show');
+};
+
 function init() {
   $hamberger.addEventListener('click', handleClickHamberger);
+  $buttonContainer.addEventListener('click', handleClickButton);
+  $pictures.forEach(($picture) =>
+    $picture.addEventListener('click', handleClickPicture)
+  );
 }
 
 init();
