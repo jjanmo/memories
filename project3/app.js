c6ca30d2f379718bf6e69932ed1bdf75da8d3ed3
jjanmo <jjanmo07@hanmail.net>
2022-07-19 23:59:13 +0900

    feat: add text typing animation in title

--- project3/app.js
+++ project3/app.js
@@ -1,4 +1,5 @@
 const aboutButtons = document.querySelectorAll('.about-button');
+const titleElem = document.querySelector('.title');
 
 const handleClickAboutBtn = (e) => {
   const targetId = e.target.dataset.target; // about p element id
@@ -16,3 +17,30 @@ function init() {
 }
 
 init();
+
+const titleTexts = [
+  '2022년 6월 29일 수요일 밤 9시 첫방송, 본방사수',
+  '조금 이상한 변호사 우영우의 대형 로펌 생존기',
+  '똑바로 읽어도 거꾸로 읽어도 우영우입니다',
+  '법무법인 한바다의 팀을 소개합니다',
+];
+
+const text = '2022년 6월 29일 수요일 밤 9시 첫방송, 본방사수';
+
+let index = 0;
+let id;
+
+const getRandomTitle = () => {
+  return titleTexts[Math.floor(Math.random() * 4)];
+};
+
+const renderTitle = () => {
+  if (titleElem.textContent.length < text.length) {
+    titleElem.textContent += text.charAt(index);
+    index++;
+
+    setTimeout(renderTitle, 100);
+  }
+};
+
+renderTitle();
