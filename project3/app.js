71b3466e4aa0b1d09bd75c0a462d2c6ff8708c16
jjanmo <jjanmo07@hanmail.net>
2022-07-20 23:03:20 +0900

    feat: add feature of random-title-rendering

--- project3/app.js
+++ project3/app.js
@@ -1,6 +1,13 @@
 const aboutButtons = document.querySelectorAll('.about-button');
 const titleElem = document.querySelector('.title');
 
+const titleTexts = [
+  '2022년 6월 29일 수요일 밤 9시 첫방송! 본방사수',
+  '이상한 변호사 우영우의 대형 로펌 생존기',
+  '똑바로 읽어도 거꾸로 읽어도 우영우입니다',
+];
+let index = 0;
+
 const handleClickAboutBtn = (e) => {
   const targetId = e.target.dataset.target; // about p element id
   const aboutElem = document.querySelector(`#${targetId}`); // about p element
@@ -10,39 +17,35 @@ const handleClickAboutBtn = (e) => {
   imageElem.classList.toggle('darken');
 };
 
+const getRandomTitle = () => {
+  return titleTexts[Math.floor(Math.random() * 3)];
+};
+
+const resetTitle = () => {
+  titleElem.textContent = '';
+  index = 0;
+
+  renderTitle(getRandomTitle());
+};
+
+const renderTitle = (title) => {
+  if (titleElem.textContent.length < title.length) {
+    titleElem.textContent += title.charAt(index);
+    index++;
+    setTimeout(() => {
+      renderTitle(title);
+    }, 100);
+  } else {
+    setTimeout(resetTitle, 3500);
+  }
+};
+
 function init() {
   aboutButtons.forEach((button) =>
     button.addEventListener('click', handleClickAboutBtn)
   );
+
+  renderTitle(getRandomTitle());
 }
 
 init();
-
-const titleTexts = [
-  '2022년 6월 29일 수요일 밤 9시 첫방송, 본방사수',
-  '조금 이상한 변호사 우영우의 대형 로펌 생존기',
-  '똑바로 읽어도 거꾸로 읽어도 우영우입니다',
-  '법무법인 한바다의 팀을 소개합니다',
-];
-
-const text = '2022년 6월 29일 수요일 밤 9시 첫방송, 본방사수';
-
-let index = 0;
-let id;
-
-const getRandomTitle = () => {
-  return titleTexts[Math.floor(Math.random() * 4)];
-};
-
-const renderTitle = () => {
-  if (titleElem.textContent.length < text.length) {
-    titleElem.textContent += text.charAt(index);
-    index++;
-
-    setTimeout(renderTitle, 100);
-  }
-};
-
-renderTitle();
-
-const test = 'commit test';
