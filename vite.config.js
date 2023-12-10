35e5e50ecd46e5a8c5c7cc1a8e95703b5dd4883e
jjanmo <jjanmo07@hanmail.net>
2023-12-10 18:25:09 +0900

    fix: fix config

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,15 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/memories/',
   build: {
     rollupOptions: {
       input: {
         home: resolve(__dirname, 'index.html'),
-        christmas: resolve(__dirname, 'christmas/index.html'),
-        'civil-war': resolve(__dirname, 'civil-war/index.html'),
-        cube: resolve(__dirname, 'cube/index.html'),
-        'extraordinary-attorney': resolve(__dirname, 'extraordinary-attorney/index.html'),
-        'prison-break': resolve(__dirname, 'prison-break/index.html'),
+        christmas: resolve(__dirname, 'src/page/christmas/index.html'),
+        'civil-war': resolve(__dirname, 'src/page/civil-war/index.html'),
+        cube: resolve(__dirname, 'src/page/cube/index.html'),
+        'extraordinary-attorney': resolve(__dirname, 'src/page/extraordinary-attorney/index.html'),
+        'prison-break': resolve(__dirname, 'src/page/prison-break/index.html'),
       },
     },
   },
