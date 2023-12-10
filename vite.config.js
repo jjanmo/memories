152dbecfb1b3d7ad84622ec00835fe05fe0bb3e7
jjanmo <jjanmo07@hanmail.net>
2023-12-10 12:53:38 +0900

    feat: add vite-build options

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,21 @@
-/** @type {import('vite').UserConfig} */
-export default {
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
   base: '/memories/',
-};
+  build: {
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, 'index.html'),
+        christmas: resolve(__dirname, 'christmas/index.html'),
+        'civil-war': resolve(__dirname, 'civil-war/index.html'),
+        cube: resolve(__dirname, 'cube/index.html'),
+        'extraordinary-attorney': resolve(__dirname, 'extraordinary-attorney/index.html'),
+        'prison-break': resolve(__dirname, 'prison-break/index.html'),
+      },
+    },
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
+  },
+});
